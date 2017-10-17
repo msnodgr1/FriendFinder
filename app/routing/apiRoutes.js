7debce425eb7c943787f1afa2c8886b4667765e5
Mark Snodgrass <msnodgr1@gmail.com>
2017-10-17 18:23:32 -0400

    final push

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,23 @@
+var path = require("path");
+
+var friends = require('../data/friends');
+
+module.exports = function(app){
+	
+
+	app.get("/api/friends", function(req, res){
+		res.json(friends);
+		console.log(friends);
+	});
+
+
+	app.post("/api/friends", function(req, res){
+		friends.push(req.body);
+		console.log(friends);
+	});
+
+	
+
+	
+
+};
